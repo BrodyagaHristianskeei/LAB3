e45d6f2bb410dee98381f3819470874c19b8c5a2
Yurii <Yurii@H-NOUT>
2022-11-26 03:24:05 +0300

    Добавьте файлы проекта.

---
+++ LAB3/LAB3.cpp
@@ -0,0 +1,20 @@
+﻿// LAB3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+//#include <iostream> Подкючает библиотеку input-output stream для вывода и ввода в консоль
+#include <Windows.h>
+//#include <Windows.h> позволяет работать с кирилицей в консоли, включает функции WinAPI sleep(), beep() и т.д
+#include <String>
+using std::string;
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a = 10;
+    int* ya;
+    ya = &a;
+    cout << "адресс указателя = " << ya << " значение указателя = " << *ya;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
